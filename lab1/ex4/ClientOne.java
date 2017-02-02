a09f9f9efa101745634ff648004c9cf2ecaaedf7
gilmoregrills <robinfarrowyonge@gmail.com>
2017-02-02 17:25:45 +0000

    add ex4 client classes

---
+++ lab1/ex4/ClientOne.java
@@ -0,0 +1,21 @@
+import java.net.*;
+import java.io.*;
+
+public class ClientOne {
+	public static void main(String[] args) {
+		startClient(args[0], args[1]);
+	}
+
+	public static void startClient(String msg, String hostname) {
+		try {
+			Socket client = new Socket(hostname, 20009);
+			System.out.println(client.getLocalPort());
+			//PrintWriter output = new PrintWriter(client.getOutputStream(), true);
+			DataOutputStream output = new DataOutputStream(client.getOutputStream());
+			output.writeUTF(msg);
+
+		} catch (IOException e) {
+			e.printStackTrace(System.out);
+		}
+	}
+}
