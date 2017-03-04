d285dc0e186ed6a0bab1df7372c9f2f473577306
gilmoregrills <robinfarrowyonge@gmail.com>
2017-03-04 11:41:28 +0000

    start actual program

---
+++ security_cwk/DecoderRing.java
@@ -0,0 +1,19 @@
+/**
+ * @Author Robin Yonge
+ * Deciphering Hex Dumps!
+ */
+import java.io.*;
+
+public class DecoderRing {
+
+	String path = "DistSysSec.txt"
+
+	public static void Main(String[] args) {
+		hexFileToChars(path);
+
+	}// function main
+
+	public static char[] hexFileToChars(String path) {
+		
+	}//function hexFileToChars
+}//class DecoderRing
