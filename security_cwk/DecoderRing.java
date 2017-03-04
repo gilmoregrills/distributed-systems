d61fab492d9fe07df554c5d6988b704adce5c88c
gilmoregrills <robinfarrowyonge@gmail.com>
2017-03-04 12:16:42 +0000

    prepare ciphertexts, add skeleton program

--- security_cwk/DecoderRing.java
+++ security_cwk/DecoderRing.java
@@ -4,16 +4,32 @@
  */
 import java.io.*;
 
+/**
+ * So I think the idea with this is to convert any hex
+ * into an array of chars so it's easy to work with 
+ * and then to xor it along with another arry of chars
+ * this leaves me plenty of flexibility regarding which 
+ * ciphertexts I want to xor together, or allows me to
+ * use any custom keys if I want to as I get closer to
+ * deciphering, intermediate results can output to files
+ */
 public class DecoderRing {
 
 	String path = "DistSysSec.txt"
 
 	public static void Main(String[] args) {
-		hexFileToChars(path);
 
 	}// function main
 
 	public static char[] hexFileToChars(String path) {
 		
 	}//function hexFileToChars
+
+	public static char[] xorWithArray(char[] text, char[] key) {
+
+	}//function xorWithArray
+
+	public static void writeToFile(char[] output) {
+
+	}//function writeToFile
 }//class DecoderRing
